fe22ee17c86e4e9d9b1f3bf3973145c1b8653baa
Aaron Nech <aaron@localhost.localdomain>
2014-03-21 13:17:21 -0700

    fixed

--- spa-app/www/js/viewmodel.js
+++ spa-app/www/js/viewmodel.js
@@ -1,11 +1,43 @@
-
+ko.bindingHandlers.slider = {
+    init: function (element, valueAccessor) {
+        // use setTimeout with 0 to run this after Knockout is done
+        setTimeout(function () {
+            // $(element) doesn't work as that has been removed from the DOM
+            var curSlider = $('#' + element.id);
+            // helper function that updates the slider and refreshes the thumb location
+            function setSliderValue(newValue) {
+                curSlider.val(newValue).slider('refresh');
+            }
+            // subscribe to the bound observable and update the slider when it changes
+            valueAccessor().subscribe(setSliderValue);
+            // set up the initial value, which of course is NOT stored in curSlider, but the original element :\
+            setSliderValue($(element).val());
+            // subscribe to the slider's change event and update the bound observable
+            curSlider.bind('change', function () {
+                valueAccessor()(curSlider.val());
+            });
+        }, 0);
+    }
+};
 
 var ViewModel = function(init) {
 	var self = this;
 	self.user = {};
 	self.formAnswers = new ko.observableArray();
-	self.questionNumber = new ko.observable();
+	self.questionNumber = new ko.observable(0);
 	self.answer = new ko.observable();
+
+    self.currentQuestion = ko.computed(function() {
+        return self.formAnswers()[self.questionNumber()];
+    });
+
+    self.showNext = ko.computed(function() {
+        return self.questionNumber() < self.formAnswers().length - 1;
+    });
+
+    self.showPrevious = ko.computed(function() {
+        return self.questionNumber() > 0;
+    });
 
 	var respondCanvas = function(){ 
 		var w = $(window).width();
@@ -17,40 +49,101 @@ var ViewModel = function(init) {
 	}
 
 	var loadVM = function(data) {
+		self.drawChart();
 		self.user = data.user;
 		self.formAnswers.push({
 			'prompt' : 'question one...',
 			'answer' : new ko.observable(5)
 		});
+		self.formAnswers.push({
+			'prompt' : 'question two...',
+			'answer' : new ko.observable(5)
+		});
+		self.formAnswers.push({
+			'prompt' : 'question three...',
+			'answer' : new ko.observable(5)
+		});
+		self.formAnswers.push({
+			'prompt' : 'question four...',
+			'answer' : new ko.observable(5)
+		});
+		self.formAnswers.push({
+			'prompt' : 'question five...',
+			'answer' : new ko.observable(5)
+		});
+		self.formAnswers.push({
+			'prompt' : 'question six...',
+			'answer' : new ko.observable(5)
+		});
+		self.formAnswers.push({
+			'prompt' : 'question seven...',
+			'answer' : new ko.observable(5)
+		});
+		self.formAnswers.push({
+			'prompt' : 'question eight...',
+			'answer' : new ko.observable(5)
+		});
+		self.formAnswers.push({
+			'prompt' : 'question nine...',
+			'answer' : new ko.observable(5)
+		});
+		self.formAnswers.push({
+			'prompt' : 'question ten...',
+			'answer' : new ko.observable(5)
+		});
+		self.formAnswers.push({
+			'prompt' : 'question eleven...',
+			'answer' : new ko.observable(5)
+		});
 	};
 
-	self.goToHome = function() {
-		$("#title > h1").text("Home");
+	var calculateScore = function() {
+		return 0;
+	}
+
+	self.goToActivity = function() {
+		$("#title > h1").text("Activity");
 		return true;
 	};
 
 	self.goToForms = function() {
-		$("#title > h1").text("Input");
+		$("#title > h1").text("New Entry");
 		return true;
 	};
 
-	self.goToStats = function() {
-		$("#title > h1").text("Statistics");
+	self.goToMe = function() {
+		$("#title > h1").text("Me");
 		self.drawChart();
 		return true;
 	};
 
 	self.nextQuestion = function() {
 		self.questionNumber(self.questionNumber() + 1);
-	}
+		$("#answer").val(self.currentQuestion().answer()).slider('refresh');
+	};
+
+	self.previousQuestion = function() {
+		self.questionNumber(self.questionNumber() - 1);
+		$("#answer").val(self.currentQuestion().answer()).slider('refresh');
+	};
 
 	self.submitAnswers = function() {
+		/*var score = calculateScore();
+		$.post(app.server + '/addscore', {'score' : score}, function() {
 
-	}
+		}, "json");*/
+	};
+
+	self.addMessage = function() {
+		/*var score = "";
+		$.post(app.server + '/addmessage', {'message' : message}, function() {
+
+		}, "json"); */
+	};
 
 	self.drawChart = function() {
 		respondCanvas();
-	}
+	};
 
 	$(window).resize(respondCanvas);
 	loadVM(init);
